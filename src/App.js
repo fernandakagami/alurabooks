321b449ce43813765c5fb53612be78d8928fda9e
Fernanda <fernandakagami@gmail.com>
2022-11-13 16:13:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './componentes/Header'
+import Pesquisa from './componentes/Pesquisa';
 import styled from 'styled-components'
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(90deg,#002F52 35%, #326589 100%);    
+  background-image: linear-gradient(90deg, #002F52 35%, #326589 100%);    
 `
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <AppContainer>
       <Header />
       <Pesquisa />
+      <UltimosLancamentos />
     </AppContainer>
   );
 }
