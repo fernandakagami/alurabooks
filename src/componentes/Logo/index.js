3051c02c61c53654b5f1da69c456759f16f81deb
Fernanda <fernandakagami@gmail.com>
2022-11-13 08:29:22 -0300

    Update index.js

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,10 +1,15 @@
+import './estilo.css'
 import logo from '../../imagens/logo.svg'
 
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt="logo da Alura Books"></img>
-            <p><strong>Alura</strong> Books</p>
+            <img 
+                src={logo}
+                alt="logo da Alura Books"
+                className='logo-img'>
+            </img>
+            <p><strong>Alura</strong>Books</p>
         </div>
     )
 }
